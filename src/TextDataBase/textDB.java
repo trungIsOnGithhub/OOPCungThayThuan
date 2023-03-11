79a8589ec96160348400ddbd5ced87176e15ff4d
Viet Trung <trung150502@gmail.com>
2023-03-11 22:43:36 +0700

    Some updates on TextDataBase

--- src/TextDataBase/textDB.java
+++ src/TextDataBase/textDB.java
@@ -9,78 +9,71 @@ import java.util.Scanner;
 
 import MenuAndItem.MenuItem;
 import OrdersAndHistory.CustomerOrder;
-// import java.text.SimpleDateFormat;
 import java.util.Date;
 /*
  *
- * @author nhom NullPointerException: Trung Nguyen va nhung nguoi ban, Lap trinh nang cao-L02-HK212
+ * @author nhom NullPointerException: Trung Nguyen va nhung nguoi ban, Lap trinh nang cao-HK212
  * 
- * Su dung 1 tap hop cac file txt lam noi luu tru du lieu lau dai cho ung dung, nhu la mot database.
- * 2 method doc menu tu file de render thanh UI va viet them mon vao menu.
- * 1 method ghi lich su don hang vao file txt.
- * 1 method kiem tra yeu cau nhap ten va mat khau(dang cho phat trien)
  */
 abstract class AbstractFile {
     private String fileName;
-    private File fileObject;
-    // private Scanner scanner;
-    // private FileWriter writer;
+    // private File fileObject;
 
     public Scanner getFileScanner() {
         try
-            { return new Scanner( new File(this.fileName) ); }
-        catch(FileNotFoundException exception) {
-            System.out.println("Kiem tra neu file "+this.fileName+" chua duoc tao");
-            exception.printStackTrace();
-            System.exit();
-        }
+            { return new Scanner(new File(this.fileName)); }
+        // catch(FileNotFoundException exception) {
+        //     System.out.println("Kiem tra neu file "+this.fileName+" chua duoc tao");
+        //     exception.printStackTrace();
+        //     System.exit(1);
+        // }
         catch(IOException exception) {
             System.out.println("Co loi khi doc file "+this.fileName);
             exception.printStackTrace();
-            System.exit();
+            System.exit(1);
         }
     }
-    public FileWriter getFileWriter() {
+    public FileWriter getFileWriter(boolean appendable) {
         try
-            { return new FileWriter( new File(this.fileName), true ); }
-        catch(FileNotFoundException exception) {
-            System.out.println("Kiem tra neu file "+this.fileName+" chua duoc tao");
-            exception.printStackTrace();
-            System.exit();
-        }
+            { return new FileWriter(new File(this.fileName), appendable); }
+        // catch(FileNotFoundException exception) {
+        //     System.out.println("Kiem tra neu file "+this.fileName+" chua duoc tao");
+        //     exception.printStackTrace();
+        //     System.exit(1);
+        // }
         catch(IOException exception) {
             System.out.println("Co loi khi doc file "+this.fileName);
             exception.printStackTrace();
-            System.exit();
+            System.exit(1);
         }
     }
 }
 
-public class HistoryFile extends AbstractFile {
+class HistoryFile extends AbstractFile {
     public HistoryFile(String historyFileName) {
         this.fileName = historyFileName;
     }
 }
 
-public class AccountsFile extends AbstractFile {
+class AccountsFile extends AbstractFile {
     public AccountsFile(String accountsFileName) {
         this.fileName = accountInfoFileName;
     }
 }
 
-public class CustomersFile extends AbstractFile {
+class CustomersFile extends AbstractFile {
     public CustomersFile(String customersFileName) {
         this.fileName = customersFileName;
     }
 }
 
-public class MenuFile extends AbstractFile {
+class MenuFile extends AbstractFile {
     public MenuFile(String menuFileName) {
         this.fileName = menuFileName;
     }
 }
 
-public abstract class AbstractFileFactory {
+abstract class AbstractFileFactory {
     public static AbstractFile getFile(String type, String fileName) {
         if( type.equalsIgnoreCase("history") )
             return new HistoryFile(fileName);
@@ -94,78 +87,92 @@ public abstract class AbstractFileFactory {
     }
 }
 
-abstract class DBFunctionalHelper {
-    final static logFileName = "log_db.txt";
-    final static configFileName = "config.txt";
 
-    // We still enable access from outside to config info of Database, but limited to read-only
-    public class configDBData {
-        private static String orderHistoryRecordFileName = "orderHistory.txt";
-        private static String customersListFileName = "customersList.txt";
-        private static String accountsInfoFileName = "accountsInfo.txt";
-        private static String menuItemsFileName = "menuItems.txt";
-    }
+abstract class DBFunctionalHelper {
+    final static String logFileName = "log_db.txt";
+    final static String configFileName = "config.txt";
+    final static int numLineInConfigFile = 4;
 
     // we will only create the abstract File object on local for saving memory
     // Because we only need those for limited number of time
+    // private class 
 
-    // get config DB data by writing config file
-    private static boolean resetDBConfigData() {
-        Scanner sc = null;
-        try { sc = new Scanner( new File(this.configFileName) ); }
-        catch(FileNotFoundException exception) {
-            System.out.println("Kiem tra neu file ,menuItems.txt chua duoc tao");
-            exception.printStackTrace();
-            return false;
-        } catch(IOException exception) {
-            System.out.println("Co loi khi doc file menuItems.txt");
-            exception.printStackTrace();
-            return false;
-        }
+    // We still enable access from outside to config info of Database, but limited to read-only
+    public static class ConfigDBData {
+        private String orderHistoryRecordFileName;
+        private String customersListFileName;
+        private String accountsInfoFileName;
+        private String menuItemsFileName;
 
-        if( sc != null ) {
-            mainMenu.clear();
-
-            String tenMon = "";
-            String giaCaStr = "";
-
-            boolean readModeSwitcher = true;
-            while( sc.hasNext() ) {
-
-                if( readModeSwitcher ) {
-                    tenMon = sc.next();
-                    // System.out.println("Ten mon: "+tenMon);
-                    
-                    readModeSwitcher = !readModeSwitcher;
-                }
-                else {
-                    giaCaStr = sc.next();
-                    // System.out.println("Gia ca: "+giaCaStr);
-                    mainMenu.add( new MenuItem(tenMon,giaCaStr) );
-                    
-                    readModeSwitcher = !readModeSwitcher;
-                }
-            }
-            sc.close();
-            return true;
-        }
-        else {
-            System.out.println("Loi khoi tao file input.txt");
-            return false;
+        public String getConfigFileName(String type) {
+            if( type.equalsIgnoreCase("history") )
+                return orderHistoryRecordFileName;
+            if( type.equalsIgnoreCase("customers") )
+                return customersListFileName;
+            if( type.equalsIgnoreCase("accounts") )
+                return accountsInfoFileName;
+            if( type.equalsIgnoreCase("menu") )
+                return menuItemsFileName;
+            return null;
         }
     }
+
+    // get config DB data by writing config file
+    private static boolean setDBConfigData() {
+        return false;
+    }
     // get config DB data by reading config file
-    public static configDBData getDBConfigData() {
-        return null;
+    public static ConfigDBData getDBConfigData() {
+        Scanner scanner = null;
+
+        try
+            { scanner = new Scanner(new File(configFileName)); }
+        catch(IOException exception) {
+            System.out.println("Co loi khi doc file "+configFileName);
+            exception.printStackTrace();
+            System.exit(1);
+        }
+
+        if(scanner == null)
+            System.exit(1);
+
+        ConfigDBData configDBData = new ConfigDBData();
+
+        int numOfLineInput = 0;
+        while(numOfLineInput < numLineInConfigFile && scanner.hasNext()) {
+            // String inputLine = scanner.nextLine();
+            // Its okay to pass the above line and read directly as we assumed our config file is well-formatted
+
+            if(numOfLineInput == 0)
+                configDBData.orderHistoryRecordFileName = scanner.nextLine();
+            else if(numOfLineInput == 1)
+                configDBData.customersListFileName = scanner.nextLine();
+            else if(numOfLineInput == 2)
+                configDBData.accountsInfoFileName = scanner.nextLine();
+            else
+                configDBData.menuItemsFileName = scanner.nextLine();
+
+            ++numOfLineInput;
+        }
+
+        if(numOfLineInput < numLineInConfigFile)
+            System.exit(1);// ill-formatted config file
+
+        scanner.close();
+
+        return configDBData;
     }
 }
 
 /*** SINGLETON CLASS ***/
 public class DBController {
-    //Singleton, One and Only Class
+    //Singleton, One and Only Object
     private DBController currentSession;
+    
+    private Date sessionInitTime;
+    private configDBData configData;
 
-    public static textDB getCurrentDBSession() {
+    public static DBController getCurrentDBSession() {
         if( this.currentSession == null ) {
             this.currentSession = new DBController();
         }
@@ -174,29 +181,15 @@ public class DBController {
 
     private DBController(Date sessionInitTime) {
         this.sessionInitTime = sessionInitTime;// dependency injection
-        this.configData = new configData();
+        this.configData = DBFunctionalHelper.getDBConfigData();
     }
-
-    private Date sessionInitTime;
-
-    private configDBData configData;
-
-    // private String orderHistoryRecordFileName = "orderHistory.txt";
-    // private File orderHistoryFile;
-    
-    // private String customersListFileName = "customersList.txt";
-    // private File customerListFile;
-
-    // private String accountsInfoFileName = "accountsInfo.txt";
-    // private File accountsInfoFile;
-
-    // private String menuItemsFileName = "menuItems.txt";
-    // private File menuItemsFile;
     
     public boolean checkLoginRequest(String username, String password) {
-        Scanner accountsScanner = AbstractFileFactory.getFile("accounts").getFileScanner();
+        Scanner accountsScanner = AbstractFileFactory.getFile("accounts",
+                                        this.configData.getConfigFileName("accounts"))
+                                    .getFileScanner();
         
-        boolean result = false;
+        boolean isSuccess = false;
         
         while( accountsScanner.hasNextLine() ){
             String lineInput = accountsScanner.nextLine();
@@ -204,78 +197,81 @@ public class DBController {
             int usernamePos = lineInput.indexOf( username );
             int passwordPos = -1;
             
-            if( usernamePos != -1 )
-                { passwordPos = lineInput.indexOf( password ); }
-            
-            if( usernamePos != -1 && passwordPos != -1 )
-                { result = true; break; }
+            if(usernamePos != -1)
+                passwordPos = lineInput.indexOf( password );
+            if(usernamePos != -1 && passwordPos != -1) {
+                isSuccess = true;
+                break;
+            }
         }
         
         accountsScanner.close();
         
-        return result;
+        return isSuccess;
     }
 
     public boolean readMenuFromDB(ArrayList<MenuItem> mainMenu) {
-        Scanner scanner = AbstractFileFactory.getFile("menu").getFileScanner();
-
+        Scanner accountsScanner = AbstractFileFactory.getFile("menu",
+                                this.configData.getConfigFileName("menu"))
+                            .getFileScanner();
         // // No need??       
         // if( scanner == null ) {
         //     System.out.println("Loi khoi tao trinh doc file");
         //     return false;
         // }
 
-        mainMenu.clear();
+        mainMenu.clear();// make sure the main menu array is empty to ensure exact output
 
-        String tenMon = "";
-        String giaCaStr = "";
+        String menuItemName = "", menuItemPriceStr = "";
 
-        boolean readModeSwitcher = true;
-
+        boolean readModeSwitchedItemName = true;
         while( scanner.hasNext() ) {
-            if( readModeSwitcher ) {
-                tenMon = scanner.next();
+            if( readModeSwitchedItemName ) {
+                menuItemName = scanner.next();
             }
-            else {
-                mainMenu.add( new MenuItem(tenMon,giaCaStr) );
-                giaCaStr = scanner.next();
+            else { // if( readMode switched to item price
+                mainMenu.add( new MenuItem(menuItemName,menuItemPriceStr) );
+                menuItemPriceStr = scanner.next();
             }
-            readModeSwitcher = !readModeSwitcher;
+            readModeSwitchedItemName = !readModeSwitchedItemName;
         }
         scanner.close();
 
         return true;
     }
+
     public boolean writeNewMenuItemIntoDB(String newItemName, String newItemPrice) {
-        FileWriter menuWriter = AbstractFileFactory.getFile("menu").getFileWriter();
+        FileWriter menuWriter = AbstractFileFactory.getFile("menu",
+                                this.configData.getConfigFileName("menu"))
+                            .getFileWriter(true);
             
-        menuWriter.write( System.lineSeparator() );
+        menuWriter.write(System.lineSeparator());
         
-        menuWriter.write( newItemName );
-        menuWriter.write( " " );
-        menuWriter.write( newItemPrice );
+        menuWriter.write(newItemName);
+        menuWriter.write(" ");
+        menuWriter.write(newItemPrice);
 
         menuWriter.close();
         
-        return true;
+        return true;// still need to return boolean as for further code, where writing data can be failed
     }
     
     public boolean writeOrderHistoryIntoDB(ArrayList<CustomerOrder> orderHistory, String dateToPrint, String timeToPrint) {
-        FileWriter orderHistoryWriter = AbstractFileFactory.getFile("history").getFileWriter();
+        FileWriter orderHistoryWriter = AbstractFileFactory.getFile("history",
+                                this.configData.getConfigFileName("history"))
+                            .getFileWriter(true);
         
-        orderHistoryWriter.write( System.lineSeparator() );
-        orderHistoryWriter.write( dateToPrint );
-        orderHistoryWriter.write( System.lineSeparator() );
-        orderHistoryWriter.write( timeToPrint );
-        orderHistoryWriter.write( System.lineSeparator() );
+        orderHistoryWriter.write(System.lineSeparator());
+        orderHistoryWriter.write(dateToPrint);
+        orderHistoryWriter.write(System.lineSeparator());
+        orderHistoryWriter.write(timeToPrint);
+        orderHistoryWriter.write(System.lineSeparator());
         
-        for( var order : orderHistory ) {
-            System.out.println( order.serializeIntoString() );
-            orderHistoryWriter.write( order.serializeIntoString() );
-            orderHistoryWriter.write( System.lineSeparator() );
+        for(var order : orderHistory) {//using var keyword to avoid changing code
+            orderHistoryWriter.write(order.serializeIntoString());
+            orderHistoryWriter.write(System.lineSeparator());
         }
-        
-        orderHistoryWriter.write( System.lineSeparator() );
+        orderHistoryWriter.write(System.lineSeparator());
 
         orderHistoryWriter.close();
         
