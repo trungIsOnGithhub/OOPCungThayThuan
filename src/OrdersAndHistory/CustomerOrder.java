b70032a4fb124a519a0fbd5a9a9ba9defa9e2f8b
Trung Nguyen <trung150502@gmail.com>
2023-03-24 12:34:50 +0700

    refactor into Decorator Pattern

--- src/OrdersAndHistory/CustomerOrder.java
+++ src/OrdersAndHistory/CustomerOrder.java
@@ -5,34 +5,22 @@ import java.util.Date;
 
 /*
  *
- * @author nhom NullPointerException: Trung Nguyen va nhung nguoi ban, Lap trinh nang cao-L02-HK212
+ * @author Trung Nguyen, Lap trinh nang cao-HK212
  *
- * Class nay mo phong lai 1 don hang trong he thong, gom co: gio giac, tong gia tri, so dien thoai nguoi mua
- * 3 constructor, voi 1 copy constructor
- * Cac getter va setter lay ra cac gia tri can thiet
- * 1 method de tao folder info
- * 1 method de xuat chuoi chua thong tin don hang
  */
 public class CustomerOrder {
-    private String timeOfOrder;
-    private Integer totalPrice;
-    private String phoneNumber;
+    protected String timeOfOrder;
+    protected Integer totalPrice;
     
-    public CustomerOrder() {
-        this.timeOfOrder = "unknown";
-        this.totalPrice = 0;
-        this.phoneNumber = "unknown";
-    }
-    public CustomerOrder(String timeOfOrder, String totalPrice, String phoneNumber) {
+    // Dependency Injection 101
+    public CustomerOrder(String timeOfOrder, String totalPrice) {
         this.timeOfOrder = timeOfOrder;
         this.totalPrice = Integer.valueOf( totalPrice );
-        this.phoneNumber = phoneNumber;
     }
     public CustomerOrder(CustomerOrder toBeCopied) {
         // Copy constructor
         this.timeOfOrder = toBeCopied.getTimeOfOrder();
         this.totalPrice = toBeCopied.getTotalPrice();
-        this.phoneNumber = toBeCopied.getPhoneNumber();
     }
     
     public String getTimeOfOrder() {
@@ -41,31 +29,45 @@ public class CustomerOrder {
     public Integer getTotalPrice() {
         return this.totalPrice;
     }
-    public String getPhoneNumber() {
-        return this.phoneNumber;
-    }
-    
+
     public String serializeIntoString() {
-        String totalPriceStr = Integer.toString( totalPrice );
-        
-        String serialized = this.timeOfOrder.concat( " " );
-        
-        serialized = serialized.concat( this.phoneNumber );
-        
-        serialized = serialized.concat( " " );
-        
-        serialized = serialized.concat( totalPriceStr );
-        
+        String serialized = this.timeOfOrder.concat( " " ).concat( String.valueOf( this.totalPrice ) );
         return serialized;
     }
     
-    public void setUpOrderInfo(String phoneNumberFromMenu, String totalPriceFromMenu) {
+    public void setUpOrderInfo(String totalPriceFromMenu) {
         // Xu ly them thong tin cho thoi gian don hang
         Date nowTime = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("hh:mm-a");
         
         this.timeOfOrder = sdf.format( nowTime );
         this.totalPrice = Integer.valueOf( totalPriceFromMenu );
-        this.phoneNumber = phoneNumberFromMenu;
     }
+    
+    public static void main(String args[]) {
+      CustomerOrder co = new CustomerOrder("8h00","78");
+      CustomerOrder cophone = new PhoneNumberDecorator(co,"1234567");
+      System.out.println(cophone.serializeIntoString());
+    }
+}
+
+class PhoneNumberDecorator extends CustomerOrder {
+    private String phoneNumber;
+
+    public PhoneNumberDecorator(CustomerOrder toBeCopied, String phoneNumber) {
+        super(toBeCopied);
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+    public String serializeIntoString() {
+        return super.serializeIntoString().concat( " " ).concat( this.phoneNumber );
+    }
+
+    // public void setUpOrderInfo(String phoneNumberFromMenu) {
+    //     super.setUpOrderInfo(this.totalPrice);
+    //     this.phoneNumber = phoneNumberFromMenu;
+    // }
 }
